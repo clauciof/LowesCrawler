4250702011cced0e1a9c4466863d1145786eb6bc
clauciof <claucio2@gmail.com>
2020-10-23 00:25:57 -0300

    Selenium adicionado para carregar conteudo html gerado dinamicamente por JavaScript

--- lowescrawler/spiders/spider.py
+++ lowescrawler/spiders/spider.py
@@ -1,5 +1,6 @@
 import scrapy
-from bs4 import BeautifulSoup
+import bs4
+from selenium import webdriver
 import pandas as pd
 import csv
 
@@ -21,44 +22,77 @@ class LowesSpider(scrapy.Spider):
     #url de inicio usada pelo spider
     start_urls = ['https://www.lowes.com/pl/Savings/4294593284?catalog=4294857973'] 
 
-    
-
     def parse(self, response):
-        soup = BeautifulSoup(response.body, 'lxml')
-        descricao_span = response.xpath('//span[@class="sc-1b7wdu0-10 fcieOM"]//text()').getall() #capturando cada <span> contendo marca e descricao dos produtos
-        hrefs = soup.find_all('a', href=True) #capturando cada <a> contendo urls
-        Ids =  response.xpath('//span[@class="tooltip-custom"]//text()').getall() #selecionando as <span> que contem os Ids e Models
-        reviews = response.xpath('//span[@class="styles__TotalWrap-sc-5hhhh2-4 gmWgqG total"]//text()').getall() #quantidade de reviews
-        stars = soup.find_all('div', class_="styles__RatingDiv-sc-5hhhh2-2 ElZBP") #estrelas de avaliaçao do produto, vao de 0 a 5
-        
-        #avalaiacoes e estrelas
-        self.estrelas = [float(s['aria-label'].split()[0]) for s in stars]
-        self.avaliacoes = [int(r) for r in reviews]
-        
-        #capturando urls válidas dos produtos
-        urls = ['https://www.lowes.com'+href['href'] for href in hrefs[23:] if '/pd/' in href['href']]
-        self.urls = list(dict.fromkeys(urls))      #eliminando duplicados   
-       
-        #separando marcas e descricao de cada <div> capturada
-        self.marcas = [descricao_span[i] for i in range(0, len(descricao_span), 3)]
-        self.titulos = [descricao_span[i+2] for i in range(0, len(descricao_span), 3)]
-        
-        #separando Id unico e o Modelo
-        self.skus = [Ids[i+1] for i in range(0,len(Ids),4)]
-        self.models = [Ids[i+3] for i in range(0,len(Ids),4)]
-
-        #salvando em um csv
-        self.create_csv(self.skus, self.models, self.marcas, self.titulos, self.estrelas, self.avaliacoes, self.urls)     
-        
-        #segue para a proxima pagina
+        urls = []
+    
         while (self.offset<=756):
+            urls.append('https://www.lowes.com/pl/Savings/4294593284?offset='+str(self.offset)+'&catalog=4294857973')
             self.offset = self.offset + 36
-            proxima_url = 'https://www.lowes.com/pl/Savings/4294593284?offset='+str(self.offset)+'&catalog=4294857973'       
-            yield scrapy.Request( proxima_url, callback=self.parse)
+            
+        for url in urls:
+            self.urls = []
+            self.titulos = []
+            self.skus = []
+            self.models = []
+            self.marcas = []
+            self.estrelas = [] 
+            self.avaliacoes = []
 
+            #configurando webdriver do selenium
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166")
+
+
+            #build do browser
+            driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='C:\\Users\\clauc\\Downloads\\CursoICMCPython\\InstaScraping\\chromedriver.exe')
+            driver.get(url)
+            soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
+
+
+            hrefs = soup.find_all('a', href=True) #capturando cada <a> contendo urls #ok
+            Ids = soup.find_all('span', class_="tooltip-custom")#ok
+            reviews = soup.find_all('span', class_="styles__TotalWrap-sc-5hhhh2-4 gmWgqG total") #ok
+            stars = soup.find_all('div', class_="styles__RatingDiv-sc-5hhhh2-2 ElZBP") #estrelas de avaliaçao do produto, vao de 0 a 5 #ok
+            marcas_span = soup.find_all('span', class_="sc-1b7wdu0-9 cjoVtZ")#ok
+            titulos_span = soup.find_all('span', class_="sc-1b7wdu0-10 fcieOM")#ok
+            
+            #avalaiacoes e estrelas
+            self.estrelas = [float(s['aria-label'].split()[0]) for s in stars]
+            self.avaliacoes = [r.text for r in reviews]
+            
+            #capturando urls válidas dos produtos
+            urls = ['https://www.lowes.com'+href['href'] for href in hrefs[23:] if '/pd/' in href['href']]
+            self.urls = list(dict.fromkeys(urls))      #eliminando duplicados   
+        
+
+            #separando marcas e titulos de cada <span> capturada
+            self.marcas = [m.text for m in marcas_span]
+            
+            for titulos_ in titulos_span:
+                aux = ''
+                for t in titulos_.text.split()[1:]:
+                    aux = aux+" "+t
+                self.titulos.append(aux)
+            
+
+            #separando Id unico e o Modelo
+            i = 0
+            for id_ in Ids:
+                if (i%2 == 0):
+                    self.skus.append(id_.text.split()[1])
+                else:
+                    self.models.append(id_.text.split()[1])
+                i = i + 1
+
+            #salvando em um csv
+            self.create_csv(self.skus, self.models, self.marcas, self.titulos, self.estrelas, self.avaliacoes, self.urls)     
+        
 
     def create_csv(self, skus, models, marcas, titulos, estrelas, avaliacoes, urls):
-        data = { 'Id':skus, 'Titulo':titulos, 'Modelo':models, 'Marca':marcas, 'Estrelas':estrelas, 'Avaliacoes':avaliacoes, 'Url':urls}
+        data = self.trata_nulos(skus, models, marcas, titulos, estrelas, avaliacoes, urls) #trata dados que vieram nulos 
         df = pd.DataFrame(data=data)
 
         with open('lowes.csv', 'a') as f:
@@ -66,4 +100,31 @@ class LowesSpider(scrapy.Spider):
         
         return 1
 
+
+    def trata_nulos(self, skus, models, marcas, titulos, estrelas, avaliacoes, urls):
+        #trata tamanhos diferentes:
+        if len(skus)<36:
+            for i in range(0, 36-len(skus)):
+                skus.append("N/D")
+        if len(models)<36:
+            for i in range(0, 36-len(models)):
+                models.append("N/D")
+        if len(marcas)<36:
+            for i in range(0, 36-len(marcas)):
+                marcas.append("N/D")
+        if len(titulos)<36:
+            for i in range(0, 36-len(titulos)):
+                titulos.append("N/D")
+        if len(estrelas)<36:
+            for i in range(0, 36-len(estrelas)):
+                estrelas.append(float(-1))
+        if len(avaliacoes)<36:
+            for i in range(0, 36-len(avaliacoes)):
+                avaliacoes.append(float(-1))
+        if len(urls)<36:
+            for i in range(0, 36-len(urls)):
+               urls.append("N/D")            
+
+        data = { 'Id':skus, 'Titulo':titulos, 'Modelo':models, 'Marca':marcas, 'Estrelas':estrelas, 'Avaliacoes':avaliacoes, 'Url':urls}
+        return data
     
